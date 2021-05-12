4c308f75dc5e701c204b7b2351b76c9d677377bb
Rose Mogusu <rosemogusu@adalabsafrica.com>
2021-05-12 14:29:09 +0300

    Add application .yml file

--- src/main/java/com/example/email/mail/service/EmailSenderService.java
+++ src/main/java/com/example/email/mail/service/EmailSenderService.java
@@ -1,4 +1,9 @@
 package com.example.email.mail.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class EmailSenderService {
+    @Autowired
+    private JavaMailSender emailSender;
 }
