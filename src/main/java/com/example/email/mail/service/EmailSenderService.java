bc6811fd08db14cc39a8006b8e794b9a3794ba3f
Rose Mogusu <rosemogusu@adalabsafrica.com>
2021-05-12 14:30:01 +0300

    Add template configuration file

--- src/main/java/com/example/email/mail/service/EmailSenderService.java
+++ src/main/java/com/example/email/mail/service/EmailSenderService.java
@@ -2,8 +2,11 @@ package com.example.email.mail.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 public class EmailSenderService {
     @Autowired
     private JavaMailSender emailSender;
+    @Autowired
+    private SpringTemplateEngine templateEngine;
 }
